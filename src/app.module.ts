f59b151f1368c57a89717c4884c38bcd909ae728
Arthur-H15 <81658735+Arthur-H15@users.noreply.github.com>
2023-04-18 22:42:43 -0300

    update docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,18 @@ import { Usuarios } from './entities/Usuarios';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql', // Tipo de banco de dados
-      host: 'db', // Host do banco de dados
-      port: 3306, // Porta do banco de dados
-      username: 'root', // Nome de usuário do banco de dados
-      password: 'password', // Senha do banco de dados
-      database: 'db', // Nome do banco de dados
-      synchronize: true, // Sincronizar automaticamente as entidades com o banco de dados (apenas para ambiente de desenvolvimento)
-      entities: [Usuarios], // Entidades do TypeORM
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql', // Tipo de banco de dados
+    //   host: 'db', // Host do banco de dados
+    //   port: 3306, // Porta do banco de dados
+    //   username: 'root', // Nome de usuário do banco de dados
+    //   password: 'password', // Senha do banco de dados
+    //   database: 'db', // Nome do banco de dados
+    //   synchronize: true, // Sincronizar automaticamente as entidades com o banco de dados (apenas para ambiente de desenvolvimento)
+    //   entities: [Usuarios], // Entidades do TypeORM
 
 
-    })
+    // })
   ],
   controllers: [AppController],
   providers: [AppService],
